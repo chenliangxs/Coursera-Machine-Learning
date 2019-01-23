f2d2620e0d5d6be7e5a7ceb7576aa086a4d8b00f
chenliangxs <chenliangxs@gmail.com>
2019-01-23 01:11:16 -0800

    init

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,53 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+%model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+c_candidate = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+sigma_candidate = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+min_error = Inf;
+x1 = [1 2 1]; x2 = [0 4 -1];
+
+for i = (1 : size(c_candidate))
+	for j = (1 : size(sigma_candidate))
+		%disp (c_candidate(i));
+		%disp (sigma_candidate(j));
+		model = svmTrain(X, y, c_candidate(i), @(x1, x2) gaussianKernel(x1, x2, sigma_candidate(j)));
+		
+		predictions = svmPredict(model, Xval);
+		error = mean(double(predictions ~= yval));
+
+		if error < min_error
+			%disp (error);
+			min_error = error;
+			C = c_candidate(i);
+			sigma = sigma_candidate(j);
+		end;
+		
+	end;
+end;
+
+
+% =========================================================================
+
+end
